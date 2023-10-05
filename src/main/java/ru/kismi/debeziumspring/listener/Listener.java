1388b7e543498f6802b9148596b09d82c046c368
mihailkiselev <kismichel17@gmail.com>
2023-10-05 16:15:57 +0300

    Fix handle message type

--- src/main/java/ru/kismi/debeziumspring/listener/Listener.java
+++ src/main/java/ru/kismi/debeziumspring/listener/Listener.java
@@ -1,5 +1,8 @@
 package ru.kismi.debeziumspring.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,12 +10,16 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 @KafkaListener(topics = "postgres.public.filter", groupId = "consumer_test_group")
 public class Listener {
 
+    private final ObjectMapper objectMapper;
+
     @KafkaHandler
-    public void handleMessage(String message) {
+    @SneakyThrows
+    public void handleMessage(Object message) {
         log.info("================ new event ============\n\n");
-        log.info(message);
+        log.info(objectMapper.writeValueAsString(message));
     }
 }
