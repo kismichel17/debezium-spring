f91defae3bfe06878a0fdc2ceb04161269271c63
mihailkiselev <kismichel17@gmail.com>
2023-10-05 16:29:29 +0300

    Without kafka + new methods

--- src/main/java/ru/kismi/debeziumspring/controller/FilterController.java
+++ src/main/java/ru/kismi/debeziumspring/controller/FilterController.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kismi.debeziumspring.entity.Filter;
 import ru.kismi.debeziumspring.repository.FilterRepository;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Slf4j
@@ -19,11 +21,33 @@ public class FilterController {
         this.filterRepository = filterRepository;
     }
 
-    @GetMapping("/")
-    public void changeFilter() {
+    @GetMapping("/1")
+    public void changeFilterDescription() {
         log.info("Change filter");
-        var filter = filterRepository.findById(1L).orElseThrow();
-        filter.setDescription(UUID.randomUUID().toString());
+        var filter = filterRepository.findAll().get(0);
+        filter.setDescription("new description-" + UUID.randomUUID());
+        filterRepository.save(filter);
+    }
+
+    @GetMapping("/2")
+    public void changeFilterName() {
+        log.info("Change filter");
+        var filter = filterRepository.findAll().get(0);
+        filter.setName("new name-" + UUID.randomUUID());
+        filterRepository.save(filter);
+    }
+
+    @GetMapping("/3")
+    public void deleteFilter() {
+        log.info("Change filter");
+        var filter = filterRepository.findAll().get(0);
+        filterRepository.delete(filter);
+    }
+
+    @GetMapping("/4")
+    public void insertNewFilter() {
+        log.info("Change filter");
+        var filter = new Filter(new Random().nextLong(), UUID.randomUUID().toString(), UUID.randomUUID().toString());
         filterRepository.save(filter);
     }
 }
